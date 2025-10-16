516ce9bc1829accef22e6c796d46e33cd8618929
requestint <studiowork1020@gmail.com>
2025-10-15 20:30:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,107 +1,57 @@
-// libraries
-import { Client, GatewayIntentBits, Role, User } from "discord.js";
-import { assert, log, table } from "node:console";
-import { request } from "node:http";
-import { time } from "node:console";
+import { Client, Events, GatewayIntentBits } from "discord.js";
+import self from "./settings.js";
+import connect, { Connect } from "./Classes/connect.js"
 
-// Json Files
-import self from './settings.js'
-import { Save } from "./Classes/save.js";
-import settings from "./settings.js";
+const token = self.PrivateToken;
 
-
-
-// Varibles
-console.warn(self)
-
-// Modules
-
-// Tables / arrays 
 const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-    ],
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
 });
 
-// Functions
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+});
 
-async function loadAndRunCommand(commandName, message) {
-  try {
-    // import { Connect } from "./Classes/Connect.js";
-    console.warn('attempting to get module : ' + `C:/Users/zjara/OneDrive/Desktop/DiscordBotCode/LiveModeration/Classes/${commandName}.js`)
-    const module = await import(`./Classes/${commandName}.js`);
-    // const module = SetPath(commandName)
-    const ClassName = module.default || module[capitalize(commandName)];
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
 
-    // if (module) {throw new Error(typeof module)}
+  switch (interaction.commandName) {
+    case "kick": {
+      const placeId = interaction.options.getInteger("placeid");
+      const userId = interaction.options.getNumber("userid");
+      const session = new Connect(placeId, userId)
 
-    if (typeof ClassName !== 'function') {
-      throw new Error(`No valid class export found in ${commandName}.js`);
+      await interaction.reply({
+        content: `Kicking user ID: \`${userId}\` from place ID: \`${placeId}\`...`,
+        ephemeral: true,
+      });
+      
+      session.start() 
+      break;
     }
 
-    const instance = new ClassName(message);
+    case "ban": {
+      // @Varibles
+      const placeId = interaction.options.getInteger("placeid");
+      const userId = interaction.options.getNumber("userid");
 
-    console.log('loaded class instance!')
+      const banLength = interaction.options.getNumber("ban-length");
+      const session = new Connect(placeId, userId, banLength)
 
-    instance.start();
-  } catch (err) {
-    console.error(err);
-    message.reply("❌ Command not found or failed to load.");
+      // Waitting for bot response
+      await interaction.reply({
+        content: `Banning user ID: \`${userId}\` from place ID: \`${placeId}\` for \`${banLength}\`...`,
+        ephemeral: true,
+      });
+
+      session.start()
+      break;
+    }
   }
-}
-
-function capitalize(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-}
-
-/*
-    Establishing connection on the bot, once connected
-    we'll establish a connection to the roblox server
-*/
-
-// * Establishing Bot Connection ->
-client.once("ready", () => {
-    console.log(`✅ Logged in as ${client.user.tag}`);
 });
 
-
-// * Message Listener
-client.on("messageCreate", (message) => {
-    // Varibles
-    const commandName = message.content.slice(1).split(' ')[0].toLowerCase();
-    const member = message.member;
-    const targetRoleName = '.';
-    
-    if (message.author.bot) return;
-
-    // Use `.some()` to check if any of the roles match the name
-    if (!member.roles.cache.some(role => role.name === targetRoleName)) {
-      return;
-    }
-
-    if (!message.content.includes(settings.prefix)) {
-      message.channel.send('Seeking command!')
-      return;
-    } 
-
-    message.reply(`Hello, ${targetRoleName}!`);
-
-    
-    try {
-        message.reply("[LIVE MODERATION] " + message.content + " recieved! ")
-
-        const newsave = new Save(message.content)
-
-        // newsave.start()
-
-        return loadAndRunCommand(commandName, message)
-    } catch (err) {
-        console.error(err);
-        message.reply("❌ Command not found or failed to load.");
-    }
-
-});
-
-client.login(self.PrivateToken);
+client.login(token);
